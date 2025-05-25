f1e605b18a2cee0843dbd5c0b05ce849352534ef
Raymond Situmorang <raymondsitumorang17@gmail.com>
2025-05-26 00:52:03 +0700

    Remove folder ecample_img

--- K_Means_app.py
+++ K_Means_app.py
@@ -19,38 +19,38 @@ def get_dominant_colors(image, k=5):
     return dominant_colors
 
 def display_color_palette(colors):
-    st.subheader("Palet Warna Dominan:")
-    cols = st.columns(len(colors))
-    for i, color in enumerate(colors):
-        with cols[i]:
-            st.markdown(
-                f"""
-                <div style="
-                    background-color: rgb({color[0]}, {color[1]}, {color[2]});
-                    width: 100px;
-                    height: 100px;
-                    border-radius: 10px;
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;
-                    color: white; /* Warna teks agar kontras */
-                    font-weight: bold;
-                    text-shadow: 1px 1px 2px black; /* Bayangan teks untuk keterbacaan */
-                    text-align: center;
-                ">
-                    <div>
-                        #{color[0]:02x}{color[1]:02x}{color[2]:02x}<br>
-                        RGB:<br>({color[0]}, {color[1]}, {color[2]})
+    with st.container(border=True):
+        st.subheader("Palet Warna Dominan:")
+        cols = st.columns(len(colors))
+        for i, color in enumerate(colors):
+            with cols[i]:
+                st.markdown(
+                    f"""
+                    <div style="
+                        background-color: rgb({color[0]}, {color[1]}, {color[2]});
+                        width: 100px;
+                        height: 100px;
+                        border-radius: 10px;
+                        display: flex;
+                        justify-content: center;
+                        align-items: center;
+                        color: white;
+                        font-weight: bold;
+                        text-shadow: 1px 1px 2px black;
+                        text-align: center;
+                    ">
+                        <div>
+                            #{color[0]:02x}{color[1]:02x}{color[2]:02x}<br>
+                            RGB:<br>({color[0]}, {color[1]}, {color[2]})
+                        </div>
                     </div>
-                </div>
-                """,
-                unsafe_allow_html=True
-            )
+                    """,
+                    unsafe_allow_html=True
+                )
 
 
 st.set_page_config(page_title="Tugas AI Color Picker", layout="centered")
 st.title("Color Picker")
-st.markdown("Upload Gambar")
 st.markdown(
     """
     <style>
@@ -64,19 +64,21 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-# Komponen Unggah File
-uploaded_file = st.file_uploader("", type=["jpg", "jpeg", "png"])
+# untuk upload file
+uploaded_file = st.file_uploader("Upload Gambar", type=["jpg", "jpeg", "png"])
 
+Contoh_Gambar_URL = "https://github.com/Skywalks567/ColorPicker/blob/main/example_img/wallpaperflare.com_wallpaper%20(5).jpg?raw=true"
+use_example = st.checkbox("Coba dengan gambar contoh")
 if uploaded_file is not None:
-    # Membaca gambar yang diunggah
+    # read gambar yang diupload
     image_bytes = uploaded_file.getvalue()
     try:
-        image = Image.open(io.BytesIO(image_bytes)).convert('RGB') # Pastikan gambar dalam format RGB
+        image = Image.open(io.BytesIO(image_bytes)).convert('RGB')
 
-        st.image(image, caption="Gambar yang Diunggah", use_column_width=True)
+        st.image(image, caption="Gambar yang Diunggah", use_container_width=True)
         with st.spinner("Menganalisis warna... ⏳"):
             dominant_colors = get_dominant_colors(image, k=5)
-        st.success("Palet Warna Berhasil Dibuat!")
+        st.success("Palet Warna Dominan Berhasil Dibuat!")
         display_color_palette(dominant_colors)
 
     except Exception as e:
